75443dd73fdd8a32c377de54807cf39d78be71cb
zaki <mhmdzki00@gmail.com>
2023-09-19 00:31:48 +0700

    menambahkan penggajian

--- src/penggajiansmp.java
+++ src/penggajiansmp.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
-
-
 public class penggajiansmp {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+        int  tunjangan = 10000; // input
+        int gajiGuru, gajiAkhir, jamMengajar;
+        String namaGuru;
 
-        int jamMengajar, tunjangan = 100000;  //input
-        double gajiGuru, gajiAkhir;
-
-        System.out.println("Masukkan gaji guru perjam");  //proses
+        System.out.println("Masukkan gaji guru perjam"); // proses
         gajiGuru = input.nextInt();
         System.out.println("Masukkan jam guru mengajar");
         jamMengajar = input.nextInt();
-        gajiAkhir = gajiGuru*jamMengajar+tunjangan;
-        System.out.println(String.format("Gaji Anda adalah: %s (sudah termasuk tunjangan)",gajiAkhir));
+        System.out.println("Masukkan nama guru");
+        namaGuru = input.next();
+        gajiAkhir = gajiGuru * jamMengajar + tunjangan;
+        System.out
+                .println(String.format("Gaji Guru %s Anda adalah: %s (sudah termasuk tunjangan)", namaGuru, gajiAkhir));
     }
 }
